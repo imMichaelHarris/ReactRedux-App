d8f0a78335d3647c819c235cad12b0a94eca75ce
Michael Harris <mhguitar1997@gmail.com>
2019-07-25 13:58:32 -0700

    Install redux-thunk

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import MissionList from "./views/MissionList";
 
 function App() {
   return (
     <div className="App">
-
+      <button>Get Space X missions</button>
+      <MissionList />
     </div>
   );
 }
