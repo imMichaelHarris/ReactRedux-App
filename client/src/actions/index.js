6fa138ffe82ee62f8b65040cc5f89fc3bf36c05a
Michael Harris <mhguitar1997@gmail.com>
2019-07-25 14:06:06 -0700

    Action creator with dispatch funtion create for getting missions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,7 @@
 export const GET_MISSIONS_START = "GET_MISSIONS_START";
 export const GET_MISSIONS_SUCCESS = "GET_MISSIONS_SUCCESS";
 export const GET_MISSIONS_FAILED = "GET_MISSIONS_FAILED";
+
+export const getMissions = () => dispatch => {
+  dispatch({ type: GET_MISSIONS_START });
+};
