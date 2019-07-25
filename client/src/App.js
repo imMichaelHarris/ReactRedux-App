4c9fd54c1c853c05a885b35da67d7220d1002cfd
Michael Harris <mhguitar1997@gmail.com>
2019-07-25 14:02:25 -0700

    Connection between app and redux store scaffold in place

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./App.css";
 import MissionList from "./views/MissionList";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <button>Get Space X missions</button>
       <MissionList />
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(App);
