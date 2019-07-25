8ae4d1b91a2699ee7a88e8f34a0ffbe676566dfb
Michael Harris <mhguitar1997@gmail.com>
2019-07-25 14:00:35 -0700

    Remove component syntax from AppWithProvider variable

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ const AppWithProvider = (
     <App />
   </Provider>
 );
-ReactDOM.render(<AppWithProvider />, document.getElementById("root"));
+ReactDOM.render(AppWithProvider, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
