6e387ddba8f2dbef437021a3b74d6972569dce3c
Michael Harris <mhguitar1997@gmail.com>
2019-07-25 14:12:51 -0700

    Axios get request add in function

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,4 +6,12 @@ export const GET_MISSIONS_FAILED = "GET_MISSIONS_FAILED";
 
 export const getMissions = () => dispatch => {
   dispatch({ type: GET_MISSIONS_START });
+  axios
+    .get("https://api.spacexdata.com/v3/missions")
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err.response);
+    });
 };
