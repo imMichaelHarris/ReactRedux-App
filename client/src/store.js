1025f3c96477648f0ae8c11ae66953f975d20044
Michael Harris <mhguitar1997@gmail.com>
2019-07-25 13:49:42 -0700

    Redux store gets reducer and applyMiddleware thunk

--- client/src/store.js
+++ client/src/store.js
@@ -1,3 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import thunk from "thunk";
+import { reducer } from "./reducers";
 
-export const store = createStore()
+export const store = createStore(reducer, applyMiddleware(thunk));
