2e583574d0a210e50d61a50cfc8cd55ebff691bb
Michael Harris <mhguitar1997@gmail.com>
2019-07-25 20:38:04 -0700

    Set container max width

--- client/src/components/Mission.jsx
+++ client/src/components/Mission.jsx
@@ -1,16 +1,58 @@
 import React, { useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import Description from "./Description";
 
 const Mission = ({ mission }) => {
   const { mission_name } = mission;
   const [show, setShow] = useState();
+
+  const useStyles = makeStyles({
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
+  const classes = useStyles();
+
   return (
     <div>
-      <header>
-        <h2>{mission_name}</h2>
-
-      </header>
-      <button onClick={() => setShow(!show)}>Show more</button>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {mission_name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary" onClick={() => setShow(!show)}>
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+      {/* <button onClick={() => setShow(!show)}>Show more</button> */}
       {show && <Description description={mission.description} />}
     </div>
   );
--- client/src/views/MissionList.js
+++ client/src/views/MissionList.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RingLoader } from "react-spinners";
-
-import Mission from '../components/Mission'
+import Grid from "@material-ui/core/Grid";
+import Mission from "../components/Mission";
 
 const MissionList = () => {
   const state = useSelector(state => state);
   return (
-    <div>
-      <RingLoader
-        sizeUnit={"px"}
-        size={100}
-        color={"#123abc"}
-        loading={state.loading}
-      />
-      {state.missions.map(mission => (
-        <Mission key={mission.mission_id} mission={mission} />
-      ))}
+    <div className="container">
+      <Grid
+        container
+        spacing={6}
+        direction="row"
+        justify="space-around"
+        alignItems="center"
+      >
+        <RingLoader
+          sizeUnit={"px"}
+          size={100}
+          color={"#123abc"}
+          loading={state.loading}
+        />
+        {state.missions.map(mission => (
+          <Grid item xs={12} sm={6} md={4} key={mission.mission_id}>
+            <Mission mission={mission} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
