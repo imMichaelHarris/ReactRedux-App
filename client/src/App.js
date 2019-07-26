a9dffe18e2215d54b2aa67157d78ef40a8b5c43f
Michael Harris <mhguitar1997@gmail.com>
2019-07-25 18:46:23 -0700

    Left navbar add

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getMissions } from "./actions";
-import { css } from '@emotion/core';
+import { css } from "@emotion/core";
+
 import "./App.css";
 
-
 import MissionList from "./views/MissionList";
+import NavBar from "./components/NavBar";
 
 const App = ({ state, getMissions }) => {
   return (
     <div className="App">
       <button onClick={getMissions}>Get Space X missions</button>
+      <NavBar />
       <MissionList />
     </div>
   );
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+
+const NavBar = () => {
+  const [open, setOpen] = useState();
+
+  const toggleDrawer = () => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setOpen(!open);
+  };
+
+  const sideList = side => (
+    <div
+      role="presentation"
+      onClick={toggleDrawer()}
+      onKeyDown={toggleDrawer()}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer()}>Open Left</Button>
+      <Drawer open={open} onClose={toggleDrawer()}>
+        {sideList("left")}
+      </Drawer>
+    </div>
+  );
+};
+
+export default NavBar;
